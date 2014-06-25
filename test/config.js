6f3d363ef14a0039f1d894da704d59bcef52e130
Espen Volden <espenv@vg.no>
2014-06-25 14:59:13 +0200

    * Refactor part of the config loading. * Add baseUrl option to the config object. * Information in the config options object should be passed to the   generated RequireJS config with paths joined with the baseUrl.

--- test/config.js
+++ test/config.js
@@ -1,5 +1,8 @@
 var test = require('tape'),
+    fs = require('fs'),
     Config = require('../lib/config');
+
+var fixture = JSON.parse(fs.readFileSync(__dirname + '/fixtures/requirejs-build.json'));
 
 test('load config from object', function(t) {
     var error = /Please supply valid config, path to config or directory to start searching up from/,
@@ -7,21 +10,24 @@ test('load config from object', function(t) {
             optimize: 'uglify2'
         };
 
-    t.plan(3);
-
-    t.throws(function() {
-        Config.loadObject();
-    }, error);
+    t.plan(4);
 
     t.throws(function() {
         Config.loadObject([]);
-    }, error);
+    }, error, 'Make sure config is object');
 
-    t.equals(Config.loadObject(scaffold).config, scaffold);
+    t.throws(function() {
+        Config.loadObject({});
+    }, /Invalid baseUrl/, 'Make sure baseUrl is valid');
+
+    var config = Config.loadObject(scaffold, __dirname);
+
+    t.looseEqual(config.config, scaffold);
+    t.equals(config.baseUrl, __dirname);
 });
 
 test('load config from file', function(t) {
-    t.plan(4);
+    t.plan(5);
 
     t.throws(function() {
         Config.loadFile('/test/foobar.json');
@@ -35,18 +41,13 @@ test('load config from file', function(t) {
         Config.loadFile('/dev/tty');
     }, /\/dev\/tty is not a file/);
 
-    t.looseEqual(Config.loadFile(__dirname + '/fixtures/requirejs-build.json').config, {
-        optimize: 'uglify2',
-        modules: {}
-    });
+    var config = Config.loadFile(__dirname + '/fixtures/requirejs-build.json');
+
+    t.looseEqual(config.config, fixture);
+    t.equals(config.baseUrl, __dirname + '/fixtures');
 });
 
 test('load config from directory', function(t) {
-    var fixture = {
-        optimize: 'uglify2',
-        modules: {}
-    };
-
     t.plan(3);
 
     t.throws(function() {
@@ -56,4 +57,15 @@ test('load config from directory', function(t) {
     t.looseEqual(Config.loadFromDirectory(__dirname + '/fixtures').config, fixture);
 
     t.looseEqual(Config.loadFromDirectory(__dirname + '/fixtures/subdir').config, fixture);
+});
+
+test('options should be passed with urls resolved', function(t) {
+    // Config in the options object should be outputed in the generated requirejs config
+    t.plan(3);
+
+    var config = Config.loadObject(fixture, __dirname).generate();
+
+    t.equal(config.optimize, fixture.options.optimize);
+    t.equal(config.mainConfigFile, __dirname + '/' + fixture.options.mainConfigFile);
+    t.equal(config.baseUrl, __dirname + '/' + fixture.options.baseUrl);
 });
