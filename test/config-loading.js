c7641e7eb139a58d390e13237619686b3a2c9018
Espen Volden <espenv@vg.no>
2014-07-22 18:07:02 +0200

    Move the config loader objects into its own file

--- test/config-loading.js
+++ test/config-loading.js
@@ -1,6 +1,6 @@
 var test = require('tape'),
     fs = require('fs'),
-    Config = require('../lib/config');
+    loader = require('../lib/loader');
 
 var fixture = JSON.parse(fs.readFileSync(__dirname + '/fixtures/requirejs-build.json'));
 
@@ -13,14 +13,14 @@ test('load config from object', function(t) {
     t.plan(4);
 
     t.throws(function() {
-        Config.loadObject([]);
+        loader.loadObject([]);
     }, error, 'make sure config is object');
 
     t.throws(function() {
-        Config.loadObject({});
+        loader.loadObject({});
     }, /Invalid baseUrl/, 'make sure baseUrl is valid');
 
-    var config = Config.loadObject(scaffold, __dirname);
+    var config = loader.loadObject(scaffold, __dirname);
 
     t.looseEquals(config.config, scaffold, 'make sure config is stored in object');
     t.equals(config.baseUrl, __dirname, 'make sure baseUrl is stored in object');
@@ -30,18 +30,18 @@ test('load config from file', function(t) {
     t.plan(5);
 
     t.throws(function() {
-        Config.loadFile('/test/foobar.json');
+        loader.loadFile('/test/foobar.json');
     }, /No such file \/test\/foobar.json/, 'loading missing file should throw');
 
     t.throws(function() {
-        Config.loadFile('/tmp');
+        loader.loadFile('/tmp');
     }, /\/tmp is not a file/, 'loading directory should throw');
 
     t.throws(function() {
-        Config.loadFile('/dev/tty');
+        loader.loadFile('/dev/tty');
     }, /\/dev\/tty is not a file/, 'loading non-file should throw');
 
-    var config = Config.loadFile(__dirname + '/fixtures/requirejs-build.json');
+    var config = loader.loadFile(__dirname + '/fixtures/requirejs-build.json');
 
     t.looseEquals(config.config, fixture, 'make sure config is stored in object');
     t.equals(config.baseUrl, __dirname + '/fixtures', 'make sure baseUrl is stored in object');
@@ -51,20 +51,20 @@ test('load config from directory', function(t) {
     t.plan(5);
 
     t.throws(function() {
-        Config.loadFromDirectory(__dirname + '/fixtures/requirejs-build.json');
+        loader.loadFromDirectory(__dirname + '/fixtures/requirejs-build.json');
     }, /is not a directory/, 'supplying file should throw');
 
     t.throws(function() {
-        Config.loadFromDirectory('/non/existing/directory');
+        loader.loadFromDirectory('/non/existing/directory');
     }, /No such directory/, 'non-existing directory should throw');
 
     t.throws(function() {
-        Config.loadFromDirectory('/');
+        loader.loadFromDirectory('/');
     }, /Could not locate requirejs-build.json/, 'missing file should throw');
 
-    t.looseEquals(Config.loadFromDirectory(__dirname + '/fixtures').config,
+    t.looseEquals(loader.loadFromDirectory(__dirname + '/fixtures').config,
                  fixture, 'make sure config is stored in object');
 
-    t.looseEquals(Config.loadFromDirectory(__dirname + '/fixtures/subdir').config,
+    t.looseEquals(loader.loadFromDirectory(__dirname + '/fixtures/subdir').config,
                  fixture, 'make sure baseUrl is stored in object');
 });
