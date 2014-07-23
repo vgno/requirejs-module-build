90aa57b8de72d32e8d3f5f4c52dc2286aebe4b2a
Espen Volden <espenv@vg.no>
2014-07-23 14:00:54 +0200

    Make config generation async

--- test/config-generation.js
+++ test/config-generation.js
@@ -10,87 +10,96 @@ test('generating invalid module', function(t) {
 
     var config = loader.object(fixture, __dirname);
 
-    t.throws(function() {
-        config.generate('non-existing');
-    }, /No config for module non-existing/, 'non-existing module should throw');
+    config.generate('non-existing', null, function(err) {
+        t.equals(err, 'No config for module non-existing');
+    });
 });
 
 test('generate requirejs module config', function(t) {
     t.plan(2);
 
-    var config = loader.object(fixture, __dirname).generate('libs');
+    var config = loader.object(fixture, __dirname);
 
-    t.equals(config.optimize, fixture.default.optimize);
-    t.looseEquals(config.include, fixture.modules.libs.include);
+    config.generate('libs', null, function(err, config) {
+        t.equals(config.optimize, fixture.default.optimize);
+        t.looseEquals(config.include, fixture.modules.libs.include);
+    });
 });
 
 test('module config should override default config', function(t) {
     t.plan(1);
 
-    var config = loader.object(fixture, __dirname).generate('shared');
+    var config = loader.object(fixture, __dirname);
 
-    t.equals(config.optimize, fixture.modules.shared.optimize);
+    config.generate('shared', null, function(err, config) {
+        t.equals(config.optimize, fixture.modules.shared.optimize);
+    });
 });
 
 test('path options should have its urls resolved', function(t) {
     // Config in the default object should be outputed in the generated requirejs config
-    var config = loader.object(fixture, __dirname).generate('libs');
-
     t.plan(3);
 
-    t.equals(config.optimize, fixture.default.optimize);
-    t.equals(config.mainConfigFile, path.join(__dirname, fixture.default.mainConfigFile));
-    t.equals(config.baseUrl, path.join(__dirname, fixture.default.baseUrl));
+    var config = loader.object(fixture, __dirname);
+
+    config.generate('libs', null, function(err, config) {
+        t.equals(config.optimize, fixture.default.optimize);
+        t.equals(config.mainConfigFile, path.join(__dirname, fixture.default.mainConfigFile));
+        t.equals(config.baseUrl, path.join(__dirname, fixture.default.baseUrl));
+    });
 });
 
 test('excludeModules', function(t) {
     t.plan(5);
 
     // excludeModules config should not be part of RequireJS config
-    var config = loader.object(fixture, __dirname),
-        libsConfig = config.generate('shared');
+    var config = loader.object(fixture, __dirname);
 
-    t.equals(libsConfig.excludeModules, undefined);
+    config.generate('shared', null, function(err, config) {
+        t.equals(config.excludeModules, undefined);
 
-    // excludeModules config should exclude other modules
-    t.looseEquals(libsConfig.exclude, ['jquery', 'lodash']);
+        // excludeModules config should exclude other modules
+        t.looseEquals(config.exclude, ['jquery', 'lodash']);
+    });
 
-    t.throws(function() {
-        config.generate('excludeModulesFaulty');
-    }, /excludeModules must be an array/);
+    config.generate('excludeModulesFaulty', null, function(err) {
+        t.equals(err, 'excludeModules must be an array');
+    });
 
-    t.throws(function() {
-        config.generate('excludeNotArray');
-    }, /exclude must be an array/);
+    config.generate('excludeNotArray', null, function(err) {
+        t.equals(err, 'exclude must be an array');
+    });
 
-    t.throws(function() {
-        config.generate('excludeMissingModule');
-    }, /Can not exclude unconfigured module/);
+    config.generate('excludeMissingModule', null, function(err) {
+        t.equals(err, 'Can not exclude unconfigured module missing-module');
+    });
 });
 
 test('directory option', function(t) {
     // Should include all files in a given directory
     t.plan(5);
 
-    var config = loader.object(fixture, __dirname + '/fixtures'),
-        dirConfig = config.generate('directory');
+    var config = loader.object(fixture, __dirname + '/fixtures');
 
-    t.equals(dirConfig.directory, undefined);
+    config.generate('directory', null, function(err, config) {
+        t.equals(config.directory, undefined);
 
-    t.looseEquals(dirConfig.include,
-                 ['directory/file1.js', 'directory/file2.js', 'directory/subdir/file3.js']);
+        t.looseEquals(config.include,
+                      ['directory/file1.js', 'directory/file2.js',
+                       'directory/subdir/file3.js']);
+    });
 
-    t.throws(function() {
-        config.generate('invalidDirectory');
-    }, /Invalid directory/);
+    config.generate('invalidDirectory', null, function(err) {
+        t.ok(err.match(/Invalid directory/));
+    });
 
-    t.throws(function() {
-        config.generate('missingDirectory');
-    }, /No such directory/);
+    config.generate('missingDirectory', null, function(err) {
+        t.ok(err.match(/No such directory/));
+    });
 
-    t.throws(function() {
-        config.generate('includeNotArray');
-    }, /include must be an array/);
+    config.generate('includeNotArray', null, function(err) {
+        t.equals(err, 'include must be an array');
+    });
 });
 
 test('invalid filters', function(t) {
@@ -99,35 +108,38 @@ test('invalid filters', function(t) {
 
     var config = loader.object(fixture, __dirname + '/fixtures');
 
-    t.throws(function() {
-        config.generate('instagram', 'foobar');
-    }, /Unknown filter/);
+    config.generate('instagram', 'foobar', function(err) {
+        t.equals(err, 'Unknown filter foobar');
+    });
 });
 
 test('inclusive filter', function(t) {
     // Create a filtered "submodule" that only includes files with a pattern
     t.plan(2);
 
-    var config = loader.object(fixture, __dirname + '/fixtures'),
-        mobileConfig = config.generate('instagram', 'mobile'),
-        allConfig = config.generate('instagram', 'all');
+    var config = loader.object(fixture, __dirname + '/fixtures');
 
-    t.looseEquals(mobileConfig.include, ['instagram/file1.mobile.js']);
+    config.generate('instagram', 'mobile', function(err, config) {
+        t.looseEquals(config.include, ['instagram/file1.mobile.js']);
+    });
 
-    t.looseEquals(allConfig.include, ['instagram/file1.js',
-                                      'instagram/file1.mobile.js',
-                                      'instagram/file2.js']);
+    config.generate('instagram', 'all', function(err, config) {
+        t.looseEquals(config.include, ['instagram/file1.js',
+                                       'instagram/file1.mobile.js',
+                                       'instagram/file2.js']);
+    });
 });
 
 test('exclusive filter', function(t) {
     // Create a filtered "submodule" that only includes files without a pattern
     t.plan(1);
 
-    var config = loader.object(fixture, __dirname + '/fixtures'),
-        mobileConfig = config.generate('instagram', 'desktop');
+    var config = loader.object(fixture, __dirname + '/fixtures');
 
-    t.looseEquals(mobileConfig.include, ['instagram/file1.js',
-                                         'instagram/file2.js']);
+    config.generate('instagram', 'desktop', function(err, config) {
+        t.looseEquals(config.include, ['instagram/file1.js',
+                                       'instagram/file2.js']);
+    });
 });
 
 test('placeholder', function(t) {
@@ -135,14 +147,15 @@ test('placeholder', function(t) {
     // files stubbed out with empty functions
     t.plan(4);
 
-    var config = loader.object(fixture, __dirname + '/fixtures'),
-        placeholderConfig = config.generatePlaceholder('instagram', 'desktop');
+    var config = loader.object(fixture, __dirname + '/fixtures');
 
-    t.ok(placeholderConfig.rawText.hasOwnProperty('instagram/file1.js'));
-    t.ok(placeholderConfig.rawText.hasOwnProperty('instagram/file2.js'));
-    t.notOk(placeholderConfig.rawText.hasOwnProperty('instagram/file1.mobile.js'));
+    config.generatePlaceholder('instagram', 'desktop', function(err, config) {
+        t.ok(config.rawText.hasOwnProperty('instagram/file1.js'));
+        t.ok(config.rawText.hasOwnProperty('instagram/file2.js'));
+        t.notOk(config.rawText.hasOwnProperty('instagram/file1.mobile.js'));
+    });
 
-    t.throws(function() {
-        config.generatePlaceholder('rawTextFaulty');
-    }, /rawText must be an object/);
+    config.generatePlaceholder('rawTextFaulty', null, function(err) {
+        t.equals(err, 'rawText must be an object');
+    });
 });
